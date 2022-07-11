cdcb3e08cca973bb81e277601bb798676daac340
kuba <atalov8603@gmail.com>
2022-07-11 16:33:36 +0600

    Initial commit

---
+++ myshop/shop/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from . models import *
+
+# Register your models here.
+from django.contrib import admin
+from . models import *
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Feature)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(CheckoutDetail)
+admin.site.register(UpdateOrder)
+admin.site.register(Contact)
